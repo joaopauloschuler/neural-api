dc45f8d6156fe4c6c5317e49b22a21cb378d0f43
joaopauloschuler <joaopauloschuler3@gmail.com>
2020-05-20 13:50:47 +0200

    Small GAN example update.

--- examples/VisualGAN/uvisualgan.pas
+++ examples/VisualGAN/uvisualgan.pas
@@ -307,15 +307,16 @@ begin
       TNNetConvolutionReLU.Create(128 * NeuronMultiplier,3,1,1,0),
       TNNetMovingStdNormalization.Create(),
       TNNetDeMaxPool.Create(2),
-      TNNetConvolutionReLU.Create(64 * NeuronMultiplier,3,1,1,0), //8x8
+      TNNetConvolutionReLU.Create(64 * NeuronMultiplier,5,2,1,0), //8x8
       TNNetMovingStdNormalization.Create(),
       TNNetConvolutionReLU.Create(64 * NeuronMultiplier,3,1,1,0),
       TNNetMovingStdNormalization.Create(),
-      TNNetConvolutionReLU.Create(64 * NeuronMultiplier,3,1,1,0),
+      TNNetDeMaxPool.Create(2),
+      TNNetConvolutionReLU.Create(64 * NeuronMultiplier,5,2,1,0), //16x16
       TNNetMovingStdNormalization.Create(),
       TNNetConvolutionReLU.Create(32 * NeuronMultiplier,3,1,1,0),
       TNNetMovingStdNormalization.Create(),
-      TNNetDeMaxPool.Create(4),
+      TNNetDeMaxPool.Create(2),
       TNNetConvolutionReLU.Create(32 * NeuronMultiplier,5,2,1,0), //32x32
       TNNetMovingStdNormalization.Create(),
       TNNetConvolutionReLU.Create(32 * NeuronMultiplier,3,1,1,0),
@@ -323,19 +324,6 @@ begin
       TNNetConvolutionLinear.Create(3,3,1,1,0),
       TNNetReLUL.Create(-40, +40) // Protection against overflow
     ]);
-    (*
-    FGenerative.AddLayer([
-       TNNetInput.Create(40, 40, 3),
-       TNNetConvolutionReLU.Create(64 * NeuronMultiplier,3,0,1,0),
-       TNNetMovingStdNormalization.Create(),
-       TNNetConvolutionReLU.Create(64 * NeuronMultiplier,3,0,1,0),
-       TNNetMovingStdNormalization.Create(),
-       TNNetConvolutionReLU.Create(128 * NeuronMultiplier,3,0,1,0),
-       TNNetMovingStdNormalization.Create(),
-       TNNetConvolutionLinear.Create(3,3,0,1,0),
-       TNNetReLUL.Create(-40, +40) // Protection against overflow
-    ]);
-    *)
     FGenerative.Layers[FGenerative.GetFirstImageNeuronalLayerIdx()].InitBasicPatterns();
   end
   else
